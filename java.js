f9e5a26d4ccede023f417b0c3518120038333e08
Eduardo20023 <145025206+Eduardo20023@users.noreply.github.com>
2024-02-13 21:07:33 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function(){
+    document.getElementById("viewAlerta").addEventListener("click",function(){
+    alert("Sabes que esta no es la respuesta correcta"); 
+    })
+})
